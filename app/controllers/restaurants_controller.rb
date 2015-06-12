ecdb5e8177bfc87fac1242833957e494f975ede0
Aaron Chen <flymaple628@gmail.com>
2015-06-12 22:11:33 +0800

    index mix new edit locak edit submit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,10 @@ class RestaurantsController < ApplicationController
 	def index
 		# @restaurants=Restaurant.all
 		@restaurants = Restaurant.page(params[:page]).per(5)
+		#給new用
+		@restaurant=Restaurant.new
+
+
 	end
 	#GET restaurant/:id
 	def show
@@ -11,7 +15,11 @@ class RestaurantsController < ApplicationController
 	end
 	#GET restaurant/:id/edit
 	def edit
+
 		# restaurant_find_one()
+		#直接將畫面展現出來
+		@restaurants = Restaurant.page(params[:page]).per(5)
+		render :action=>:index
 	end
 	#GET restaurant/new
 	def new
@@ -19,24 +27,27 @@ class RestaurantsController < ApplicationController
 	end
 	#POST restaurant/
 	def create
+		@restaurants = Restaurant.page(params[:page]).per(5)
 		@restaurant=Restaurant.new(restaurant_form)
+
+
+
 		if @restaurant.save
 			flash[:notice]="You have already save."
 			redirect_to restaurants_path
-
 		else
-			render :action=>:new
+			render :action=>:index
 		end
 	end
 	#PATCH restaurant/:id/edit
 	def update
 		# restaurant_find_one
-
+		@restaurants = Restaurant.page(params[:page]).per(5)
 		if @restaurant.update(restaurant_form)
 			flash[:notice]="You have already save."
 			redirect_to restaurants_path
 		else
-			render :action=>:edit
+			render :action=>:index
 		end
 	end
 	#DELETE restaurant/:id
