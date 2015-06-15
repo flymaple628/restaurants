421bcafb14ceafff953d2a02c87961d015d4c449
Aaron Chen <flymaple628@gmail.com>
2015-06-15 14:28:18 +0800

    iHowr review cut uselessmark

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,28 +1,23 @@
 class MenusController < ApplicationController
 	before_action :menu_get_one,:only=>[:edit,:destroy,:update,:show]
 	before_action :menu_get_list,:only=>[:edit,:index]
-	#GET menu/
+
 	def index
 		menu_get_list
-		if params[:id].nil?
-			@menu=Menu.new
-		else
+		if params[:id]
 			menu_get_one
+		else
+			@menu=Menu.new
 		end
 	end
-	#GET menu/:id
+
 	def show
 
 	end
-	#GET menu/:id/edit/
-	def edit
-		render :action=>:index
-	end
 
-	#DELETE munu/:id
 	def destroy
-			@menu.destroy
-			redirect_to menus_path
+		@menu.destroy
+		redirect_to menus_path(:page=>params[:page])
 	end
 
 	#POST menu/:id
@@ -35,23 +30,25 @@ class MenusController < ApplicationController
 		end
 
 	end
-	#PATCH menu/:id
+
 	def update
-			if @menu.update(menu_juge_require)
-			redirect_to menu_path()
-			else
-				render :action=>:index
-			end
+		if @menu.update(menu_juge_require)
+			redirect_to menus_path(:page=>params[:page])
+		else
+			render :action=>:index
+		end
 	end
 
-
 	private
+
 	def menu_get_list
 		@menus=Menu.page(params[:page]).per(5)
 	end
+
 	def menu_get_one
 		@menu=Menu.find(params[:id])
 	end
+
 	def menu_juge_require
 		params.require(:menu).permit(:name,:price,:remove_time,:ready_time,:ordderby,:tag)
 	end
